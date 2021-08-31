a9002dcd76fb1f368fa7df065a3274b55cde6679
audryf <84256033+audryf@users.noreply.github.com>
2021-08-31 12:18:19 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+// variables
 const express = require('express');
-
+const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
@@ -8,6 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// api routes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// file routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
@@ -16,6 +23,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+
+// server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
