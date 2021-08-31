5abe8236432babb95a9c2e0b39ce6db6959545c8
audryf <84256033+audryf@users.noreply.github.com>
2021-08-31 13:11:30 -0500

    POST route, add id to notes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // variables
 const express = require('express');
 const notes = require('./db/db.json');
+const path = require('path');
+const { nanoid } = require('nanoid');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +15,13 @@ app.use(express.static('public'));
 // api routes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+// NEXT, this POST route needs to read the new note, save as a JSON object and give it a unique id, then add to the db.json file
+app.post('/api/notes', (req, res) => {
+    req.body.id = nanoid(10);
+    const newNote = req.body;
+    console.log(newNote);
+    res.json(newNote);
 });
 
 // file routes
