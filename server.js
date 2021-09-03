96ccff347ab39dc1cde13385ffd767fc2ef967fc
audryf <84256033+audryf@users.noreply.github.com>
2021-09-02 21:45:23 -0500

    MVP

--- server.js
+++ server.js
@@ -2,34 +2,52 @@
 const express = require('express');
 const notes = require('./db/db.json');
 const path = require('path');
-const { nanoid } = require('nanoid');
+const fs = require('fs');
+// nanoid to create unique id (using date/time for now)
+// const { nanoid } = require('nanoid'); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // api routes
-app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => { // tested, works
     res.json(notes);
 });
 // NEXT, this POST route needs to read the new note, save as a JSON object and give it a unique id, then add to the db.json file
-app.post('/api/notes', (req, res) => {
-    req.body.id = nanoid(10);
+app.post('/api/notes', (req, res) => { // tested, works
+    req.body.id = (new Date()).getTime();
     const newNote = req.body;
+    notes.push(newNote);
+    fs.writeFile('db/db.json', JSON.stringify(notes), err => {
+        if (err) throw err;
+        return true;
+    });
     console.log(newNote);
     res.json(newNote);
 });
 
+// still working on this...
+// app.delete('/api/notes/:id', (req, res) => {
+//     console.log(req.params.id);
+//     console.log(notes)
+//                 SPECIFICALLY THIS AREA...
+//         notes = notes.filter(function(note) {
+//             return note.id !== req.params.id
+//         });
+//         console.log(notes)
+// });
+
 // file routes
-app.get('/notes', (req, res) => {
+app.get('/notes', (req, res) => {   // tested, works
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req, res) => {   // tested, works
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
